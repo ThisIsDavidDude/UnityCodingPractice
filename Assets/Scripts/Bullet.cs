21f977fb7d8d66f59a21f18d437d6ae6ce9afc5f
ThisIsDavidDude <davingotdeveloper@gmail.com>
2025-10-31 23:07:59 -0400

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] public float bulletSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * bulletSpeed);
+        if (transform.position.y > 6.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
