8aecccb87b183961225a2a9109389f4a1358f923
ThisIsDavidDude <davingotdeveloper@gmail.com>
2025-11-01 01:06:39 -0400

    Code Update
    Edited enemy and player code

---
+++ Assets/Scripts/Enemy2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * 6f);
+        if (transform.position.x > 7.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
